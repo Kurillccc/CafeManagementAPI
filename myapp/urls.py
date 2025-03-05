bcd601dbb9805e97d0387f44b7e6944b6446399e
Kurillccc <89049125360@mail.ru>
2025-03-05 20:03:53 +0300

    Added actions after clicking on the add button and added a transition to page 2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import page1
+from .views import page1, page2
 
 urlpatterns = [
     path('page1/', page1, name='page1'),
+    path('page2/', page2, name='page2'),
 ]
